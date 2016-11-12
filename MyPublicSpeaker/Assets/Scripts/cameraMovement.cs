2206024cd5deee028b535ea79e055f6fb34afffe
Cynthia Martinez <spiralcin99@aol.com>
2016-11-12 08:38:44 -0800

    First Commit
    Added project files

---
+++ MyPublicSpeaker/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMovement : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	void Start () {
+        offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LastUpdate () {
+        transform.position = player.transform.position + offset;
+	
+	}
+}
